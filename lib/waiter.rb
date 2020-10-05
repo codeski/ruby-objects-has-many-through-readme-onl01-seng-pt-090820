74f53a0f0778f6edcfc6dc80a733c67103d4c299
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-10-05 20:53:49 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,17 @@
 class Waiter
+  
+  attr_accessor :name, :yrs_exp 
+  
+  @@all = []
+  
+  def initialize 
+    @name 
+    @yrs_exp
+    @@all << self
+  end
+  
+  def self.all 
+    @@all 
+  end
 
 end
